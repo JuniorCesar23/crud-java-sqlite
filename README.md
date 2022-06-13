e57bc9af3d13af775165e2646d555eaaf3eb8d3c
JuniorCesar23 <cesarck36@gmail.com>
2022-06-13 18:57:25 -0300

    Criada a entidade Pessoa

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 <h1>CRUD simples com Java e Sqlite</h1>
+
+> status: <h3>Em construção</h3>
+
+<h2>Tecnologias utilizadas: </h2>
+<li>Java</li>
+<li>Maven</li>
+<li>SQLite</li>
+<li>Lombok</li>
---
+++ src/main/java/com/junior/entities/Pessoa.java
@@ -0,0 +1,13 @@
+package com.junior.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Pessoa {
+
+    private String nome;
+    private String email;
+
+}
