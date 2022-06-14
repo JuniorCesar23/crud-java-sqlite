f9d77954774a66764b90b73809d671bf88413905
JuniorCesar23 <cesarck36@gmail.com>
2022-06-14 10:38:52 -0300

    Classe para criar a tabela

--- src/main/java/com/junior/App.java
+++ src/main/java/com/junior/App.java
@@ -4,11 +4,14 @@ import java.sql.Connection;
 
 import com.junior.connections.FecharConexao;
 import com.junior.connections.IniciarConexao;
+import com.junior.database.CriarTabela;
 
 public class App {
     public static void main(String[] args) {
     
         Connection connection = IniciarConexao.iniciarConexao();
+
+        CriarTabela.criarTabela(connection);
 
         FecharConexao.fecharConexao(connection);
     
---
+++ src/main/java/com/junior/database/CriarTabela.java
@@ -0,0 +1,29 @@
+package com.junior.database;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class CriarTabela {
+
+    public static void criarTabela(Connection connection) {
+
+        try {
+
+            Statement statement = connection.createStatement();
+
+            String criarTabela = "CREATE TABLE pessoa ("
+                    + "id INT AUTO_INCREMENT PRIMARY KEY, "
+                    + "nome VARCHAR(50) NOT NULL, "
+                    + "email VARCHAR(50) NOT NULL"
+                    + ")";
+
+            statement.execute(criarTabela);
+
+            System.out.println("Tabela criada");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
